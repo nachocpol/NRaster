99cdf095d2a8deee9a3ddd3bfc3cae34f62687bd
Nacho Cortizo <nachocpol@gmail.com>
2019-05-29 21:14:37 +0100

    Typo.

--- GenerateSolution.bat
+++ GenerateSolution.bat
@@ -1,6 +1,6 @@
 "./Dependencies/Premake/premake5.exe" --file=SolutionGenerator.lua vs2017
 
-xcopy /y "%~dp0Dependencies\SDL2-2.0.9\lib\x64\SDL2.dll" "%~dp0Paco\Binaries\x64\Debug\"
+xcopy /y "%~dp0Dependencies\SDL2-2.0.9\lib\x64\SDL2.dll" "%~dp0Binaries\x64\Debug\"
 xcopy /y "%~dp0Dependencies\SDL2-2.0.9\lib\x64\SDL2.dll" "%~dp0Binaries\x64\Release\"
       
 pause
